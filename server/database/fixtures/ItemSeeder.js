78ced98aaea5f261ead99c9a5fc26f203b7fdebb
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-08-04 14:10:26 +0200

    First commit

--- server/database/fixtures/ItemSeeder.js
+++ server/database/fixtures/ItemSeeder.js
@@ -2,12 +2,11 @@ const AbstractSeeder = require("./AbstractSeeder");
 
 // Import seeders that must be executed before this one
 // Follow your foreign keys to find the right order ;)
-const UserSeeder = require("./UserSeeder");
 
 class ItemSeeder extends AbstractSeeder {
   constructor() {
     // Call the constructor of the parent class (AbstractSeeder) with appropriate options
-    super({ table: "item", truncate: true, dependencies: [UserSeeder] });
+    super({ table: "item" });
   }
 
   // The run method - Populate the 'item' table with fake data
@@ -18,7 +17,6 @@ class ItemSeeder extends AbstractSeeder {
       // Generate fake item data
       const fakeItem = {
         title: this.faker.lorem.word(), // Generate a fake title using faker library
-        user_id: this.getRef(`user_${i}`).insertId, // Get the insertId of the corresponding user from UserSeeder
       };
 
       // Insert the fakeItem data into the 'item' table
