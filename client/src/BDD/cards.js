f970f28d86c92e1db625d1facc0a85a8b937e897
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-08-04 15:52:36 +0200

    started json bdd

---
+++ client/src/BDD/cards.js
@@ -0,0 +1,23 @@
+const Activities = [
+  {
+    type: "face",
+    description:
+      "pirate",
+      number : null,
+  },
+  {
+    type: "face",
+    description:
+      "sirens",
+      number : null,
+  },
+  {
+    type: "colors",
+    description:
+      "violet",
+    number : 1,
+    
+  },
+];
+
+export default Activities;
